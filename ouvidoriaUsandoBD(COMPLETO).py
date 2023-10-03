9b557a7b2afeb53801e2affec4737eb9765c46e9
Arthur Silva <arthur.v.silva05@gmail.com>
2023-10-02 21:45:26 -0300

    Código Final com Métodos

--- ouvidoriaUsandoBD(COMPLETO).py
+++ ouvidoriaUsandoBD(COMPLETO).py
@@ -5,62 +5,30 @@ conn = abrirBancoDados("127.0.0.1", "root", "011198", "ouvidoria_final")
 opcao = 0
 
 while opcao != 5:
-    print()
-    print("----- Sistema de Ouvidoria -----")
-    print("\n1) Listar Ocorrências\n2) Cadastrar Ocorrência\n3) Alterar Ocorrência\n4) Excluir Ocorrência\n5) Sair\n")
-    print("--------------------------------")
+    menu()
     opcao = int(input("-Qual das opções você deseja realizar? "))
-    print()
 
     sqlconsulta = "select codigo, ocorrencia from ocorrencias"
     sqlListagem = listarBancoDados(conn, sqlconsulta)
 
     if opcao == 1:
-        if len(sqlListagem) == 0:
-            print("-Sem ocorrências, adicione uma primeiro")
-        else:
-            print("-Lista de ocorrências:")
-            for ocorrencia in sqlListagem:
-                print("-", ocorrencia[1])
+        listagem(sqlListagem)
 
     elif opcao == 2:
-        novaOcorrencia = input("-Digite a ocorrência: ")
-        if novaOcorrencia.strip():
-            sqlInsert = "insert into ocorrencias (ocorrencia) values (%s)"
-            dados = [novaOcorrencia]
-            metodoInsert = insertNoBancoDados(conn, sqlInsert, dados)
-            print("--------------------------------")
-            print("-Ocorrência adicionada com sucesso!")
-        else:
-            print("-Nenhuma ocorrência informada")
+        ocorrenciadigitada = input("-Digite sua ocorrência: ")
+        nova_ocorrencia = ocorrenciadigitada
+        cadastro(conn, nova_ocorrencia)
 
     elif opcao == 3:
-        if len(sqlListagem) == 0:
-            print("-Sem ocorrências, adicione uma usando a Opção 2")
-        else:
-            print("-As ocorrências cadastradas no sistema são:\n")
-            for codigo, ocorrencia in sqlListagem:
-                print("-Código", codigo, "corresponde a ocorrência: ", ocorrencia)
-
-            print("-----------------------------------------------------")
-            codigoAlterar = input("-Digite o código da ocorrência que você quer alterar: ")
-            ocorrenciaUpdate = input("-Digite a ocorrência atualizada: ")
-            sqlUpdate = "update ocorrencias set ocorrencia = %s where codigo = %s"
-            dados = [ocorrenciaUpdate, codigoAlterar]
-            metodoUpdate = atualizarBancoDados(conn, sqlUpdate, dados)
-            print("-Mensagem alterada com sucesso!")
+        listagemIndice(sqlListagem)
+        codigoAlterar = input("-Digite o código da ocorrência que você quer alterar: ")
+        ocorrenciaUpdate = input("-Digite a ocorrência atualizada: ")
+        update(conn, ocorrenciaUpdate, codigoAlterar)
 
     elif opcao == 4:
-        print("-As ocorrências cadastradas no sistema são:\n")
-        for codigo, ocorrencia in sqlListagem:
-            print("-Código", codigo, "corresponde a ocorrência: ", ocorrencia)
-
-        print("--------------------------------------------")
+        listagemIndice(sqlListagem)
         codigoPesquisado = int(input("-Digite o código da ocorrência para remover: "))
-        sqldelete = "delete from ocorrencias where codigo = %s"
-        dados = [codigoPesquisado]
-        metodoDelete = excluirBancoDados(conn, sqldelete, dados)
-        print("\n-Ocorrência removida com sucesso!")
+        delete(conn, codigoPesquisado)
 
     elif opcao != 5:
         print("-Opção Inválida. Escolha uma opção válida.")
